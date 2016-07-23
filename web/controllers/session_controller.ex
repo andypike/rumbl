261c98c54c8faf83070d0cfc2e12e26fa1aa4b9b
Andy Pike <andy@andypike.com>
2016-07-23 23:12:11 +0100

    Add videos for users and clean up authetication

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -12,11 +12,11 @@ defmodule Rumbl.SessionController do
       {:ok, conn} ->
         conn
         |> put_flash(:info, "Welcome back!")
-        |> redirect to: page_path(conn, :index)
+        |> redirect(to: page_path(conn, :index))
       {:error, _reason, conn} ->
         conn
         |> put_flash(:error, "Invalid username/password combination")
-        |> render "new.html"
+        |> render("new.html")
     end
   end
 
