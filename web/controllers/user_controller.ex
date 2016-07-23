144dea9780ef1ed1e36d60873104a33b7e2fdd1a
Andy Pike <andy@andypike.com>
2016-07-23 09:59:02 +0100

    Login newly created users

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,6 +2,7 @@ defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
 
   alias Rumbl.User
+  alias Rumbl.Auth
 
   plug :authenticate when action in [:index, :show]
 
@@ -26,6 +27,7 @@ defmodule Rumbl.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Auth.login(user)
         |> put_flash(:info, "#{user.name}, created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
