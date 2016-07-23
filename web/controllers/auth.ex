d12c8461fee96702cb316f5c49d4b2edded7f0e7
Andy Pike <andy@andypike.com>
2016-07-23 09:21:55 +0100

    Authenticate a request from the user id stored in session

---
+++ web/controllers/auth.ex
@@ -0,0 +1,17 @@
+defmodule Rumbl.Auth do
+  @moduledoc """
+  Authenticates a request from a :user_id in the session
+  """
+
+  import Plug.Conn
+
+  def init(opts) do
+    Keyword.fetch!(opts, :repo)
+  end
+
+  def call(conn, repo) do
+    user_id = get_session(conn, :user_id)
+    user = user_id && repo.get(Rumbl.User, user_id)
+    assign(conn, :current_user, user)
+  end
+end
--- web/models/user.ex
+++ web/models/user.ex
@@ -31,11 +31,10 @@ defmodule Rumbl.User do
 
   defp put_pass_hash(changeset) do
     case changeset do
-      %Ecto.Changeset{
-        valid?: true,
-        changes: %{password: pass}
-      } -> put_change(changeset, :password_hash, Bcrypt.hashpwsalt(pass))
-      _ -> changeset
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
     end
   end
 end
